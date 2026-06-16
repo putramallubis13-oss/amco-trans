d44bc452abc787ff1a8371a9e5939793587ec147
putramallubis13-oss <putramallubis13@gmail.com>
2026-06-16 23:04:38 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'hamco-trans-v1';
+const urlsToCache = [
+  '/amco-trans/index.html',
+  '/amco-trans/driver.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
